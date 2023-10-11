9f7fb809c1af06db43c39269433596d34812cc20
John Heger <heger7@gmail.com>
2023-10-11 15:54:14 -0500

    Completed Lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// Write your code here 
+
+const divide = function() {
+    return 2000/100
+}
+
+const square = parameter1 => parameter1*parameter1;
+
+const add = (parameter2, parameter3) => parameter2 + parameter3;
+
